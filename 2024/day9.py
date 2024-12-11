0f35f06dacc176cdea94cfb802192840365f4138
Carlos Lopez <cjlc@hotmail.com>
2024-12-10 22:47:19 -0700

    day 9 part 1

--- 2024/day9.py
+++ 2024/day9.py
@@ -1,31 +1,23 @@
 from openData import getData
 
-def divideArray(arr):
-    array1 = []
-    array2 = []
-
-    for index, number in enumerate(arr):
-        if index % 2 == 0:
-            array1.append(number)
-        else:
-            array2.append(number)
-
-    return (array1, array2)
-
 if __name__ == "__main__":
 
-    #input = list(getData("day9InputTestSmall.txt")[0])
+    # input = getData("day9InputTestSmall.txt")
     input = getData("day9Input.txt")
+    # input = getData("testData.txt")
     memBlocks = list(input[0])
 
-    (filesBlocks, freeBlocks) = divideArray(memBlocks)
+    filesBlocks = memBlocks[::2]
+    freeBlocks = memBlocks[1::2]
 
-    files = ''
+    files = ""
 
     for index, file in enumerate(filesBlocks):
-        files += str(index) * int(file)
+        # for i in range(int(file)):
+        #     files.append(index)
+        files += (str(index)+',') * int(file)
 
-    files = list(files)
+    files = files.split(',')[:-1]
 
     block = []
 
@@ -40,35 +32,10 @@ if __name__ == "__main__":
                 block.append(files.pop())
             else:
                 break
-
-    # freeMemory = False
-
-    # block = ''
-
-    # currentIndex = 0
-
-    # for number in input:        
-    #     block += ('.' if freeMemory else str(currentIndex)) * int(number)
-    #     if not freeMemory:
-    #         currentIndex += 1
-    #     freeMemory = not freeMemory
     
-    # block = list(block)
+    while files:
+        block.append(files.pop(0))
 
-    # currentIndex = 0
-
-    # for index, mem in enumerate(block):
-    #     if not any([c.isdigit() for c in block[index:-1]]):
-    #         break
-    #     if mem == '.':
-    #         lastCurrentPosition = block[-1-currentIndex]
-    #         while lastCurrentPosition == '.':
-    #             currentIndex += 1
-    #             lastCurrentPosition = block[-1-currentIndex]
-    #         block[index] = lastCurrentPosition
-    #         block[-1-currentIndex] = '.'
-    #         currentIndex += 1
-    
     checkSum = sum([index * int(mem) for index, mem in enumerate(block)])
 
     print("Result: ", checkSum)
