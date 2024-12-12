404b180d72f1f00d9d06d81d4a294bf5b51882be
CJX3M <cjx3m@hotmail.com>
2024-12-12 12:40:20 -0700

    day 11 solution

--- 2024/openData.py
+++ 2024/openData.py
@@ -9,3 +9,12 @@ def getData(fileName):
         data = fileObject.read().split('\n')
 
     return data
+
+def getData(day, test):
+    test = "Test" if test else ""
+    filename = f"{day}Input{test}.txt"
+    strPath = os.path.join(sys.path[0], filename)
+    with open(strPath) as fileObject:
+        data = fileObject.read().split('\n')
+
+    return data
