1f3c7faab5de064d79a71fbe644506236e61e474
CJX3M <cjx3m@hotmail.com>
2024-12-15 02:10:21 -0700

    day12 solution

--- 2024/day12.py
+++ 2024/day12.py
@@ -1,52 +1,109 @@
 from openData import getData
-from copy import deepcopy
+
+def deepSearch(current, directions, area, region, grid, limits):
+    for direction in directions:
+        neighboor = tuple((a+b for a,b in zip(current, direction)))
+        if neighboor not in grid:
+            continue
+        if inbound(neighboor, limits) and neighboor not in region and grid[neighboor] == area:
+            region.append(neighboor)
+            grid.pop(neighboor, None)
+            region = deepSearch(neighboor, directions, area, region, grid, limits)
+    return region                
+
+def inbound (coords, limits): 
+    return 0 <= coords[0] <= limits[0] and 0 <= coords[1] <= limits[1]
+
 
 if __name__ == "__main__":
+    print()
+    input = [list(r) for r in getData("12", False)]
 
-    input = [list(r) for r in getData("12", True)]
+    directions = [[-1, 0], #up
+                  [ 0,-1], #left
+                  [ 0, 1], #right
+                  [ 1, 0]] #down
 
-    garden = dict()
+    grid = { ( rowI, colI ): char
+            for rowI, row in enumerate( input )
+            for colI, char in enumerate( row ) }
+    xmin, *_, xmax = sorted( { x for x, y in grid.keys() } )
+    ymin, *_, ymax = sorted( { y for x, y in grid.keys() } )
 
-    maxRows = len(input)
-    maxCols = len(input[0])
+    limit = (xmax, ymax)
 
-    for rowI, r in enumerate(input):
-        for colI, area in enumerate(r):
-            if area not in garden:
-                garden[area] = {
-                    "region": [[rowI, colI]],
-                    "area": 1,
-                    "perimeter": 0
-                }
-            else:
-                garden[area]["region"].append([rowI, colI])
-                garden[area]["area"] += 1
-                    
-    directions = [[ 1,0], #down
-                  [ 0,1], #right
-                  [-1,0], #up
-                  [ 0,-1]]#left
+    total1 = 0
+    total2 = 0
 
-    inbound = lambda coords: 0 <= coords[0] < maxRows and 0 <= coords[1] < maxCols
+    for coord in [c for c in grid]:
+        if coord not in grid:
+            continue
+        region = []
+        area = grid[coord]
+        grid.pop(coord, None)
+        region.append(coord)
+        region = deepSearch(coord, directions, area, region, grid, limit)
+        squares = len(region)
+        perimeter = 0        
+        corners = 0
+        if squares == 1:
+            perimeter = 4
+            corners = 4
+        else:
+            for coords in region:
+                for direction in directions:
+                    neighboor = tuple((a+b for a,b in zip(coords, direction)))
+                    if not inbound(neighboor, limit):
+                        perimeter += 1
+                    elif input[neighboor[0]][neighboor[1]] != area:
+                        perimeter += 1
+                
+                getSide = lambda coords, direction: tuple((a+b for a,b in zip(coords, direction)))
+                checkSide = lambda side, limit, input, area: not inbound(side, limit) or input[side[0]][side[1]] != area
 
-    total = 0
+                side = getSide(coords, directions[0])
+                up = checkSide(side, limit, input, area)
+                side = getSide(coords, directions[3])
+                down = checkSide(side, limit, input, area)
+                side = getSide(coords, directions[1])
+                left = checkSide(side, limit, input, area)
+                side = getSide(coords, directions[2])
+                right = checkSide(side, limit, input, area)
 
-    for area in garden.keys():
-        for coords in garden[area]["region"]:
-            regionNeighboor = False
-            for direction in directions:
-                neighboor = [a+b for a,b in zip(coords, direction)]
-                if not inbound(neighboor):
-                    garden[area]["perimeter"] += 1
-                    continue
-                if input[neighboor[0]][neighboor[1]] != area:
-                    garden[area]["perimeter"] += 1
-                else:                    
-                    regionNeighboor = True
-            if not regionNeighboor:
-                garden[area]["area"] -= 1
-            if garden[area]["area"] == 0:
-                garden[area]["area"] = 1
-        total += garden[area]["perimeter"] * garden[area]["area"]
+                if up and left:
+                    corners += 1
+                if up and right:
+                    corners += 1
+                if down and left:
+                    corners += 1
+                if down and right:
+                    corners += 1
 
-    print("\r\nResult part 1: ", total)
+                side = getSide(coords, [-1, 1])
+                innerCorner = checkSide(side, limit, input, area)
+                if not up and not right and innerCorner:
+                    corners += 1
+
+                side = getSide(coords, [-1, -1])
+                innerCorner = checkSide(side, limit, input, area)
+                if not up and not left and innerCorner:
+                    corners += 1
+
+
+                side = getSide(coords, [1, -1])
+                innerCorner = checkSide(side, limit, input, area)
+                if not down and not left and innerCorner:
+                    corners += 1
+
+                side = getSide(coords, [1, 1])
+                innerCorner = checkSide(side, limit, input, area)
+                if not down and not right and innerCorner:
+                    corners += 1
+
+        total1 += perimeter * squares
+        total2 += corners * squares
+        print(f"\n\rRegion {area} has an area of {squares} and a perimeter of {perimeter} for a total of {squares * perimeter}", end="")
+        print(f"\n\rRegion {area} has an area of {squares} and {corners} sides for a total of {corners * squares}", end="")
+
+    print("\r\nResult part 1: ", total1)
+    print("\r\nResult part 2: ", total2)
