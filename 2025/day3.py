cc986b993145545107c6658b864afa19fcd348b5
Carlos Lopez <cjlc@hotmail.com>
2025-12-07 13:55:30 -0700

    day 3 solved

--- 2025/day3.py
+++ 2025/day3.py
@@ -1,6 +1,6 @@
 import openData
 
-input = openData.getData(3, True)
+input = openData.getData(3, False)
 input = [list(map(int, list(line))) for line in input]
 
 values = []
@@ -15,10 +15,11 @@ for volt in input:
 
     switch = []
     value = 0
-    for i in range(12-1, -1, -1):
-        offset = i  if i > 0 else 0
-        digit  = volt[:offset]
-        value = (value + 10) + digit
+    for i in range(-12+1, 1, 1):
+        offset = i  if i != 0 else None
+        digit  = max(volt[:offset])
+        value = (value * 10) + digit
+        volt = volt[volt.index(digit) + 1:]
     switches.append(value)
 
 print("Part 1 result: ", sum(values))
