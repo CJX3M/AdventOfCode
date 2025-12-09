07f670ab497d4fa484f26914d50334164482e18f
CJX3M <cjx3m@hotmail.com>
2025-12-09 10:40:47 -0700

    day6 solution

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,56 +1,82 @@
 import openData
 
-input = openData.getData(6, True)
-
-offset = 0
+useTest = False
 
 matrix = []
+
+input = [l.split(" ") for l in openData.getData(6, useTest)]
+
+for i in range(len(input)):
+    input[i] = [x for x in input[i] if x != '']
+
+rotateMatrix = []
+
+for i in range(len(input[0])):
+    newRow = []
+    for j in range(len(input)):
+        newRow.append(input[j][i])
+    rotateMatrix.append(newRow)
+
+results = []
+for row in rotateMatrix:
+    operation = row[-1]
+    value = 0
+    for val in row[:-1]:    
+        if operation == "+":
+            value += int(val)
+        elif operation == "*":
+            if value == 0:
+                value = 1
+            value *= int(val)
+    results.append(value)
+
+print("Part 1:", sum(results))
+
+input = openData.getData(6, useTest)
+offset = 0
 row = []
 currentCol = 0
-while currentCol < len(input[:-1]):
-    #usar la ultima linea de input para determinar el offset
+colOffsets = []
+
+for cIndex, c in enumerate(input[-1]):
+    offset += 1
+    if cIndex == len(input[-1]) - 1:
+        colOffsets.append(offset)
+        break
+    if c == " " and input[-1][cIndex + 1] != " ":
+        colOffsets.append(offset)
+        offset = 0
+
+for offset in colOffsets:  
     for line in input:
-        if offset == 0:
-            for cIndex, c in enumerate(line):
-                if c == " ":
-                    offset += 1
-        row.append(line[currentCol:currentCol + offset])
-        # for c in range(0, len(line), offset + 1):
-        #     marker = line[c:offset + c]
-        #     row.append(marker)
-
+        row.append(line[currentCol:currentCol + offset][::-1])
     matrix.append(row)
-    currentCol += offset + 1
-    offset = 0
-print(matrix)    
-        
+    row = []
+    currentCol += offset
 
+results = []
 
-# input = [l.split(" ") for l in openData.getData(6, False)]
+for line in matrix:
+    values = []
+    for currCol in range(len(line[0])):
+        value = ''
+        for col in line:
+            c = col[currCol]
+            if c == " ":
+                continue
+            elif c in ["+", "*"]:
+                if value != '':
+                    values.append(int(value))
+                    value = ''
+                if c == "+":
+                    results.append(sum(values))
+                else:
+                    results.append(eval('*'.join(map(str, values))))
+            else:
+                value += c
+        if value != '':
+            values.append(int(value))
+    
 
-# for i in range(len(input)):
-#     input[i] = [x for x in input[i] if x != '']
-
-# rotateMatrix = []
-
-# for i in range(len(input[0])):
-#     newRow = []
-#     for j in range(len(input)):
-#         newRow.append(input[j][i])
-#     rotateMatrix.append(newRow)
-
-# results = []
-# for row in rotateMatrix:
-#     operation = row[-1]
-#     value = 0
-#     for val in row[:-1]:    
-#         if operation == "+":
-#             value += int(val)
-#         elif operation == "*":
-#             if value == 0:
-#                 value = 1
-#             value *= int(val)
-#     results.append(value)
-
-# print("Part 1:", sum(results))
+print("Part 2:", sum(results))
 
