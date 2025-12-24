bf77bafd2b77c15c06afd53957838cfe6a3275f1
CJX3M <cjx3m@hotmail.com>
2025-12-24 13:21:21 -0700

    optimized with numpy

--- 2025/day8.py
+++ 2025/day8.py
@@ -1,49 +1,54 @@
 import openData
 import math
+import numpy as np
 from collections import defaultdict
 
 useTest = 1 == 0
 
 input = [tuple(map(int, row.split(','))) for row in openData.getData(8, useTest)]
+points = np.array(input)
+N = len(input)
 
-dist = []
-
-for i, (x1, y1, z1) in enumerate(input):
-    for j, (x2, y2, z2) in enumerate(input):
-        if i < j:
-            d = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
-            dist.append((d, i, j))
+# Compute all pairwise distances efficiently
+indexI, indexJ = np.triu_indices(N, k=1)
+diffs = points[indexI] - points[indexJ]
+dists = np.sqrt(np.sum(diffs**2, axis=1))
+edges = sorted(zip(dists, indexI, indexJ))
 
 connections = 10 if useTest else 1000
 
-parent = {key: key for key in range(len(input))}
+parent = list(range(N))
 
 def findSet(i):
-    return i if parent[i] == i else findSet(parent[i])
+    while parent[i] != i:
+        parent[i] = parent[parent[i]]  # Path compression
+        i = parent[i]
+    return i
 
 def mergeSet(i, j):
     parent[findSet(i)] = findSet(j)
 
 numConns = 0
+lastMerge = None
 
-for conn, (d, i, j) in enumerate(sorted(dist)):
+for conn, (d, i, j) in enumerate(edges):
     if conn == connections:
         circuits = defaultdict(int)
-        for node in parent:
+        for node in range(N):
             circuits[findSet(node)] += 1
         conns = sorted(circuits.values(), reverse=True)
         Part1 = math.prod(conns[:3])
         print("Part 1:", Part1)
-
     root_i = findSet(i)
     root_j = findSet(j)
     if root_i != root_j:
         numConns += 1
         mergeSet(i, j)
-        if numConns == len(input) - 1:
-            last_merge = (i, j)
+        if numConns == N - 1:
+            lastMerge = (i, j)
 
-i, j = last_merge
-Part2 = input[i][0] * input[j][0]
-print("Part 2:", Part2)
+if lastMerge:
+    i, lastMergej = lastMerge
+    Part2 = input[i][0] * input[j][0]
+    print("Part 2:", Part2)
 
